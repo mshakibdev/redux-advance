c033f1adf896132be8398ad5f80fa2bad88fc546
uicub <flow.uicub@gmail.com>
2022-02-22 15:01:24 +0600

    useEffect added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
+
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 
 function App() {
   const showCart = useSelector((state) => state.ui.isCartVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch("https://redux-0101-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
